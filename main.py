3c4bf12d26eaa1274c590dfbe51f4ce81deb1235
kabust <tulyulyuk91@gmail.com>
2024-09-12 22:26:44 +0200

    updated main.py with consideration of new features

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import os
 import time
+import numpy as np
 
-from dotenv import load_dotenv
-from openai import OpenAI
-
-
-load_dotenv()
-TOKEN = os.getenv("OPENAI_TOKEN")
+from config import client
+from audio_input import AudioHandler
 
 
 def main() -> None:
-    client = OpenAI(api_key=TOKEN)
+    audio_handler = AudioHandler(client=client)
+    prompt_from_speech = audio_handler.speech_to_text()
+
     start = time.time()
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are a language tutor."},
             {
                 "role": "user",
-                "content": "Write a haiku about recursion in programming."
+                "content": prompt_from_speech
             }
         ]
     )
     print("Took:", time.time() - start)
-    print(completion.choices[0].message)
+    print(completion.choices[0].message.content)
 
 
 if __name__ == '__main__':
