6904ffab5c428806644675c1fd4e5a2daad6d3bb
kabust <tulyulyuk91@gmail.com>
2024-09-19 21:13:13 +0200

    added "/completion/" endpoint

--- gpt/config.py
+++ gpt/config.py
@@ -6,15 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("OPENAI_TOKEN")
+ASSISTANT_ID = os.getenv("ASSISTANT_ID")
 
 client = OpenAI(api_key=TOKEN)
-
-assistant = client.beta.assistants.create(
-    name="Language Tutor",
-    instructions="""
-    You are a language tutor, use simple language as a real teacher and
-    try to lead the conversation. Imagine that you have a 1 on 1 in-person meeting. 
-    Also, emphasise grammatical and logical mistakes, but keep the dialog casual and engaging.
-    """,
-    model="gpt-4o-mini",
-)
+assistant_id = ASSISTANT_ID
+# assistant = client.beta.assistants.create(
+#     name="Language Tutor",
+#     instructions="""
+#     You are a language tutor, use simple language as a real teacher and
+#     try to lead the conversation. Imagine that you have a 1 on 1 in-person meeting.
+#     Also, emphasise grammatical and logical mistakes, but keep the dialog casual and engaging.
+#     """,
+#     model="gpt-4o-mini",
+# )
--- gpt/routes.py
+++ gpt/routes.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from dependencies import get_db
 from gpt.gpt_client_handler import GPTClientHandler
-from gpt.config import client
-
+from gpt.config import client, assistant_id
 
 router = APIRouter()
 gpt_client_handler = GPTClientHandler(client=client)
@@ -12,22 +11,22 @@ gpt_client_handler = GPTClientHandler(client=client)
 
 @router.post("/completion/")
 async def completion(prompt: str, db: AsyncSession = Depends(get_db)):
-    client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-                {"role": "system", "content":
-                    """
-                    You are a language tutor, use simple language as a real teacher and
-                    try to lead the conversation. Imagine that you have a 1 on 1 in-person meeting. 
-                    Also, emphasise grammatical and logical mistakes, but keep the dialog casual and engaging.
-                    """
-                 },
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ],
+    thread = client.beta.threads.create()
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+        instructions=prompt
     )
+
+    if run.status == 'completed':
+        messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+        response = messages.data[0].content[0].text.value
+        print(response)
+        return {"response": response}
+    else:
+        print(run.status)
 
 
 @router.post("/transcription/")
